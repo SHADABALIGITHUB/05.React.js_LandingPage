609fc946376a4e3f36b2ca7b67ee43680365e1ab
shadab <shad46243@gmail.com>
2024-06-03 02:18:06 +0530

    prizing card

--- src/Components/Routes/Pricing/Pricing.jsx
+++ src/Components/Routes/Pricing/Pricing.jsx
@@ -16,51 +16,27 @@ const Pricing = () => {
       ]);
     return (
        
-       <div className="py-16 bg-white md:p-10">
-
-<div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-
-<div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-
-      {
-          data.map((item,index)=>(
-
-            <div className="md:5/12 lg:w-5/12" key={index}>
-            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                {item.plan}
-            </h2>
-            <p className="mt-6 text-gray-600">
-                {item.price}
-            </p>
-            <ul className="mt-4 text-gray-600">
-                {item.features.map((feature,index)=>(
+        <div className="py-16 bg-white md:p-10">
+        <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+          <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+            {data.map((item, index) => (
+              <div className="md:5/12 lg:w-5/12" key={index}>
+                <h2 className="text-2xl text-customRed font-bold md:text-4xl">
+                  {item.plan}
+                </h2>
+                <p className="mt-6 text-black">
+                  {item.price}
+                </p>
+                <ul className="mt-4 text-gray-600">
+                  {item.features.map((feature, index) => (
                     <li key={index}>{feature}</li>
-                ))}
-            </ul>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
         </div>
-
-
-
-
-          ))
-
-
-
-      }
-
-        
-
-        
-
-
-
-    </div>
-
-             
-
-        </div>
-    
-</div>
+      </div>
     );
 }
 
